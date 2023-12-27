09a8b2c37d8060a6f53798b944ca1e599fa3ffc9
BrendanRomanDev <roman.bp15@gmail.com>
2023-12-27 09:58:58 -0500

    Init Commit

---
+++ wezterm.lua
@@ -0,0 +1,93 @@
+-- wezterm will watch for file changes and reload on save
+-- for manual config refresh: CTRL + SHIFT + R
+local wezterm = require("wezterm")
+
+-- This table will hold the configuration.
+local config = {}
+
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
+
+-- Initial window size
+config.initial_rows = 40
+config.initial_cols = 120
+
+--  Styling
+config.color_scheme = "Solarized Dark Higher Contrast"
+config.font = wezterm.font("SpaceMono Nerd Font Mono")
+config.font_size = 13
+-- -- disable ligatures
+config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
+config.window_background_opacity = 0.65
+config.macos_window_background_blur = 30
+-- -- inactive pane styling
+config.inactive_pane_hsb = {
+	saturation = 1,
+	brightness = 0.4,
+}
+
+-- Updates
+config.status_update_interval = 10000
+
+-- Keymap
+config.keys = {
+	-- Panes
+	-- -- splits
+	{ key = "b", mods = "CTRL | CMD", action = wezterm.action.SplitHorizontal },
+	{ key = "u", mods = "CTRL | CMD", action = wezterm.action.SplitVertical },
+	{ key = "-", mods = "CTRL | CMD", action = wezterm.action.CloseCurrentPane({ confirm = true }) },
+	-- -- resize
+	{ key = "h", mods = "CTRL | CMD", action = wezterm.action.AdjustPaneSize({ "Left", 10 }) },
+	{ key = "l", mods = "CTRL | CMD", action = wezterm.action.AdjustPaneSize({ "Right", 10 }) },
+	{ key = "k", mods = "CTRL | CMD", action = wezterm.action.AdjustPaneSize({ "Up", 5 }) },
+	{ key = "j", mods = "CTRL | CMD", action = wezterm.action.AdjustPaneSize({ "Down", 5 }) },
+	{ key = "0", mods = "CTRL", action = wezterm.action.ResetFontAndWindowSize },
+	-- -- selection
+	{ key = "n", mods = "CTRL | CMD", action = wezterm.action.ActivatePaneDirection("Next") },
+	{ key = "p", mods = "CTRL | CMD", action = wezterm.action.ActivatePaneDirection("Prev") },
+	{ key = ",", mods = "CTRL | CMD", action = wezterm.action.PaneSelect },
+	-- Rotation / Swap
+	{ key = "Tab", mods = "CTRL | CMD", action = wezterm.action.RotatePanes("CounterClockwise") },
+}
+
+-- Window swapping with cmd alt + windowNum
+for i = 1, 8 do
+	-- CMD+ALT + number to activate that window
+	table.insert(config.keys, {
+		key = tostring(i),
+		mods = "CMD | CTRL",
+		action = wezterm.action.ActivateWindow(i - 1),
+	})
+end
+
+-- Relocating your tab to a different order
+for i = 1, 8 do
+	-- CTRL+ALT + number to move to that position
+	table.insert(config.keys, {
+		key = tostring(i),
+		mods = "CTRL|ALT",
+		action = wezterm.action.MoveTab(i - 1),
+	})
+end
+
+--  Update tab titles based on cwd
+wezterm.on("update-status", function(window)
+	local active_pane = window:active_tab():active_pane()
+
+	if not active_pane then
+		wezterm.log_info("no pane sorry")
+		return
+	end
+
+	local cwd = active_pane:get_current_working_dir()
+	local curr_tab_title = active_pane:tab():get_title()
+	local new_tab_title = string.match(cwd, "/([^/\\]+)[/\\]?$")
+
+	if curr_tab_title ~= new_tab_title then
+		window:active_tab():set_title(new_tab_title)
+	end
+end)
+
+-- and finally, return the configuration to wezterm
+return config
---
+++ zshrc
@@ -0,0 +1,45 @@
+
+# Paths
+export PATH=/opt/homebrew/bin:$PATH
+export PATH="$PATH:/Applications/Visual Studio 
+Code.app/Contents/Resources/app/bin"
+export PATH="$PATH:/Applications/WezTerm.app/Contents/MacOS"
+export PATH=${PATH}:/usr/local/mysql/bin
+export EDITOR=nvim
+
+## Opts
+## vi keybindings in linux shell
+set -o vi
+setopt autocd
+setopt HIST_IGNORE_ALL_DUPS
+
+# will execute jump in zsh
+eval "$(jump shell zsh)"
+
+# aliases and functions
+alias wezconf='cd && nvim .wezterm.lua'
+alias lovey='love .'
+alias sql='mysql -u root -p';
+alias jts='j thrive/server'
+alias jtw='j thrive/web'
+alias jte='j thrive/ehr'
+alias jtso='j thrive/server && nvim'
+alias jtwo='j thrive/web && nvim'
+alias jteo='j thrive/ehr && nvim'
+alias dcu='docker compose up'
+alias dcd='docker compose down'
+
+# repeat command history
+fh() {
+  print -z $( ([ -n "$ZSH_NAME" ] && fc -l 1 || history) | fzf +s --tac --height "50%" | sed -E 's/ *[0-9]*\*? *//' | sed -E 's/\\/\\\\/g')
+}
+
+# fuzzy enter file from $EDITOR
+fe() {
+  IFS=$'\n' files=($(fzf-tmux --query="$1" --multi --select-1 --exit-0))
+  [[ -n "$files" ]] && ${EDITOR:-vim} "${files[@]}"
+}
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
